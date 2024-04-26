acd2ccf65b723cdc93c850054e311b2b58bb1e04
somekite <87897782+somekite@users.noreply.github.com>
2024-04-26 22:57:40 +0530

    Update g.js

--- js/g.js
+++ js/g.js
@@ -1,1 +1,8 @@
 var g_url = "test";
+
+var params = new URLSearchParams(window.location.search);
+
+  if(params.has('g')) {
+      var gid = params.get('g');
+      document.getElementById('f1').src= g_url + '/?g=' + gid;
+  }
